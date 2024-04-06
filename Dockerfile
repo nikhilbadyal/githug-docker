6fd6ace10efcac2739ffbd809f029a0013afad4f
Nikhil Badyal <nikhill773384@gmail.com>
2024-04-06 17:20:51 +0530

    Pinned ruby version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:alpine
+FROM ruby:3.1.4-alpine
 RUN gem install githug:0.5.0 && apk add git
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
 RUN echo y | githug
--- run_githug.sh
+++ run_githug.sh
@@ -1,2 +1,13 @@
-docker build -t githug:latest .
-docker run -it --name githug githug /bin/sh
+#!/bin/bash
+
+container_name="githug"
+
+# Check if the container exists
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+    # Container exists, start it
+    docker exec -it "$container_name" sh
+else
+    # Container does not exist, build the Docker image and run a new container
+    docker build -t "$container_name":latest .
+    docker run -it --name "$container_name" "$container_name" /bin/sh
+fi
